d22801c838013fd0305178f4b438183a6c5eda7e
akhilamartia <akhila.martia@gmail.com>
2020-07-15 00:02:19 +0530

    Updates

---
+++ precache-manifest.4c3d48e78107938219688d6ba46b6cc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95fee0c1fc8cc103937e0caf36bbc073",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9e99b9f35484750662bd",
+    "url": "/monsters-rolodex/static/css/main.825eb177.chunk.css"
+  },
+  {
+    "revision": "b31136ca6c05b330dc51",
+    "url": "/monsters-rolodex/static/js/2.680e7d0c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.680e7d0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e99b9f35484750662bd",
+    "url": "/monsters-rolodex/static/js/main.d8481bdc.chunk.js"
+  },
+  {
+    "revision": "bfe0afb2c995894ce67b",
+    "url": "/monsters-rolodex/static/js/runtime-main.7bbaf8e8.js"
+  }
+]);
